aa8eb96e65f3a800ad17abb7f6c9028b40407fc4
kankuro12 <cms111000111@gmail.com>
2022-03-27 10:46:06 +0545

    stock managed

---
+++ database/migrations/2022_03_27_104418_add_data_to_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDataToItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            //
+            $table->decimal('stock',18,3)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->integer('stock')->change();
+        });
+    }
+}
