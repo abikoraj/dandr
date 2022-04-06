3ab79ca8f8d503bf2db9d42acc053992a5f20de4
kankuro12 <cms111000111@gmail.com>
2022-04-06 14:16:20 +0545

    promo sms added to sidebar

--- resources/views/admin/layouts/menu_admin.blade.php
+++ resources/views/admin/layouts/menu_admin.blade.php
@@ -108,6 +108,7 @@
                     <li><a href="{{ route('admin.customer.home') }}" class="waves-effect waves-block">List </a></li>
                     <li><a href="{{ route('admin.customer.payment.index') }}" class="waves-effect waves-block">Payment</a></li>
                     <li><a href="{{ route('admin.customer.credit-list.index') }}" class="waves-effect waves-block">Due  SMS</a></li>
+                    <li><a href="{{ route('admin.customer.promo') }}" class="waves-effect waves-block">Promo  SMS</a></li>
                 </ul>
             </li>
             @if (env('use_pos',false))
