826acc65f0b3d2960e6edac406e237b8c329e2e3
kankuro12 <cms111000111@gmail.com>
2022-07-16 16:08:02 +0545

    import old finished

--- app/Console/Commands/ImportOld.php
+++ app/Console/Commands/ImportOld.php
@@ -44,73 +44,67 @@ class ImportOld extends Command
     public function handle()
     {
         try {
-            $users=json_decode( file_get_contents(public_path('exports/users.json')));
-            // foreach ($users as $key => $user) {
-            //     try {
-            //         //code...
-            //         User::insert((array)$user);
-            //         echo $user->name.PHP_EOL;
-            //     } catch (\Throwable $th) {
-            //         echo $th->getMessage();
-            //     }
-            // }
-            // $centers=json_decode( file_get_contents(public_path('exports/centers.json')));
-            // foreach ($centers as $key => $center) {
-            //     Center::insert((array)$center);
-            //     echo $center->name;
-            // }
-            // $farmers=json_decode( file_get_contents(public_path('exports/farmers.json')));
-            // foreach ($farmers as $key => $farmer) {
-            //     try {
-            //         Farmer::insert((array)$farmer);
-            //         echo $farmer->no.PHP_EOL;
-            //         //code...
-            //     } catch (\Throwable $th) {
-            //         echo $th->getMessage();
-
-            //     }
-            // }
-            // $distributers=json_decode( file_get_contents(public_path('exports/distributers.json')));
-            // foreach ($distributers as $key => $dis) {
-            //     try {
-            //         Distributer::insert((array)$dis);
-            //         echo $dis->rate .PHP_EOL;
-            //         //code...
-            //     } catch (\Throwable $th) {
-            //         echo $th->getMessage();
-
-            //     }
-            // }
-                $now=Carbon::now()->toDateTimeString();
-             $distributers_balance=json_decode( file_get_contents(public_path('exports/distributers_balance.json')));
-            foreach ($distributers_balance as $key => $bal) {
-                if($bal->amount>0){
-                        try {
-                            Ledger::insert([
-                                "title"=>"Aalya",
-                                "amount"=>$bal->amount,
-                                "user_id"=>$bal->user_id,
-                                "type"=>$bal->type,
-                                "year"=>2079,
-                                "month"=>4,
-                                "session"=>1,
-                                "identifire"=>119,
-                                "date"=>20790401,
-                                "created_at"=>$now,
-                                "updated_at"=>$now
-                            ]);
-                            //code...
-                        } catch (\Throwable $th) {
-                            echo $th->getMessage();
-
-                        }
+            $users = json_decode(file_get_contents(public_path('exports/users.json')));
+            foreach ($users as $key => $user) {
+                try {
+                    //code...
+                    User::insert((array)$user);
+                    echo $user->name.PHP_EOL;
+                } catch (\Throwable $th) {
+                    echo $th->getMessage();
+                }
+            }
+            $centers=json_decode( file_get_contents(public_path('exports/centers.json')));
+            foreach ($centers as $key => $center) {
+                Center::insert((array)$center);
+                echo $center->name;
+            }
+            $farmers=json_decode( file_get_contents(public_path('exports/farmers.json')));
+            foreach ($farmers as $key => $farmer) {
+                try {
+                    Farmer::insert((array)$farmer);
+                    echo $farmer->no.PHP_EOL;
+                    //code...
+                } catch (\Throwable $th) {
+                    echo $th->getMessage();
 
                 }
             }
+            $distributers=json_decode( file_get_contents(public_path('exports/distributers.json')));
+            foreach ($distributers as $key => $dis) {
+                try {
+                    Distributer::insert((array)$dis);
+                    echo $dis->rate .PHP_EOL;
+                    //code...
+                } catch (\Throwable $th) {
+                    echo $th->getMessage();
 
-
-
-
+                }
+            }
+            $now = Carbon::now()->toDateTimeString();
+            $distributers_balance = json_decode(file_get_contents(public_path('exports/distributers_balance.json')));
+            foreach ($distributers_balance as $key => $bal) {
+                if ($bal->amount > 0) {
+                    try {
+                        Ledger::insert([
+                            "title" => "Aalya",
+                            "amount" => $bal->amount,
+                            "user_id" => $bal->user_id,
+                            "type" => $bal->type,
+                            "year" => 2079,
+                            "month" => 4,
+                            "session" => 1,
+                            "identifire" => 119,
+                            "date" => 20790401,
+                            "created_at" => $now,
+                            "updated_at" => $now
+                        ]);
+                        //code...
+                    } catch (\Throwable $th) {
+                        echo $th->getMessage();
+                    }
+                }
+            }
         } catch (\Throwable $bth) {
             //bthrow $bth;
             echo $bth->getMessage();
