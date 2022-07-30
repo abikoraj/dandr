46abc30c0a2e79b4fe15ef8adf3cb144d32a3897
kankuro12 <cms111000111@gmail.com>
2022-07-30 07:05:44 +0545

    dashboard managed

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,47 +1,45 @@
 @extends('admin.layouts.app')
-@section('title','Dashboard')
+@section('title', 'Dashboard')
 @section('css')
 @endsection
 @section('content')
-<script>
-    datas=[];
-    const range={!! json_encode($range) !!};
-    console.log(range,'range');
-</script>
+    <script>
+        datas = [];
+        const range = {!! json_encode($range) !!};
+        console.log(range, 'range');
+    </script>
 
 
-@include('admin.modalmenu')
-<hr>
-<h5 class="d-flex justify-content-between">
-    <span>
+    @include('admin.modalmenu')
 
-        General Report of {{_nepalidate($today)}}
-    </span>
-    <span>
-        <a href="{{route('admin.summary.index')}}">View More </a>
-    </span>
-</h5>
+    @if (env('use_farmer', false))
+        <hr>
+        <h5 class="d-flex justify-content-between">
+            <span>
 
-@if (env('use_farmer',false))
-
-    <hr>
-    <div class="row" id="datas">
-        @include('admin.index.milk')
-        @include('admin.index.sales')
-    </div>
+                General Report of {{ _nepalidate($today) }}
+            </span>
+            <span>
+                <a href="{{ route('admin.summary.index') }}">View More </a>
+            </span>
+        </h5>
+        <hr>
+        <div class="row" id="datas">
+            @include('admin.index.milk')
+            @include('admin.index.sales')
+        </div>
     @endif
 @endsection
 @section('js')
-    <script src="{{asset('assets/js/chart.js')}}"></script>
+    <script src="{{ asset('assets/js/chart.js') }}"></script>
     <script>
-        $all=[];
+        $all = [];
         console.log(datas);
-        $(document).ready(function () {
+        $(document).ready(function() {
             datas.forEach(ele => {
 
             });
         });
-
     </script>
 
 
